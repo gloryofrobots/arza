177331f9170c485734c51d1c942b4632c2e1a4a2
gloryofrobots <gloryofrobots@gmail.com>
2015-07-27 23:32:08 +0300

    test_integers

--- src/test/types/test_integer.c
+++ src/test/types/test_integer.c
@@ -1,6 +1,3 @@
-static void _ints_test_compare(OState* state, OAny int1, OAny int2) {
-
-}
 static void Test_Integer(void) {
 	printf("OINT_MAX ="OBIN_INTEGER_FORMATTER" OINT_MIN="OBIN_INTEGER_FORMATTER, OINT_MAX, OINT_MIN);
 	/*TODO overflows*/
@@ -30,10 +27,10 @@ static void Test_Integer(void) {
 	CU_ASSERT_STRING_EQUAL(OString_cstr(S, var1), "-42");
 	var1 = otostring(S, OInteger(0));
 	CU_ASSERT_STRING_EQUAL(OString_cstr(S, var1), "0");
-	var1 = otostring(S, intmax);
+	var1 = otostring(S, OInteger(2147483647));
 	CU_ASSERT_STRING_EQUAL(OString_cstr(S, var1), "2147483647");
-	var1 = otostring(S, intmin);
-	CU_ASSERT_STRING_EQUAL(OString_cstr(S, var1), "-2147483648");
+	var1 = otostring(S, OInteger(-2147483647));
+	CU_ASSERT_STRING_EQUAL(OString_cstr(S, var1), "-2147483647");
 
 	/*__tobool__ */
 	CU_ASSERT_TRUE(OAny_isTrue(otobool(S,intmin)));
@@ -114,28 +111,40 @@ static void Test_Integer(void) {
 	CU_ASSERT_EQUAL(OAny_toInt(oinvert(S, int0)), -1);
 	CU_ASSERT_EQUAL(OAny_toInt(oinvert(S, intmax)), OINT_MIN);
 	CU_ASSERT_EQUAL(OAny_toInt(oinvert(S, intmin)), OINT_MAX);
-	/*__invert__*/
+	/*__leftshift__*/
+	CU_ASSERT_EQUAL(OAny_toInt(oleftshift(S, int1, int0)), 1);
+	CU_ASSERT_EQUAL(OAny_toInt(oleftshift(S, int0, int1)), 0);
+	CU_ASSERT_EQUAL(OAny_toInt(oleftshift(S, OInteger(214), OInteger(12))), 876544);
+	CU_ASSERT_EQUAL(OAny_toInt(oleftshift(S, OInteger(-8888), OInteger(7))), -1137664);
+	/*__rightshift__*/
+	CU_ASSERT_EQUAL(OAny_toInt(orightshift(S, int1, int0)), 1);
+	CU_ASSERT_EQUAL(OAny_toInt(orightshift(S, int0, int1)), 0);
+	CU_ASSERT_EQUAL(OAny_toInt(orightshift(S, OInteger(23), OInteger(2))), 5);
+	CU_ASSERT_EQUAL(OAny_toInt(orightshift(S, OInteger(-1233), OInteger(3))), -155);
 
-	/*TODO tofloat*/
+	/*__mod__*/
+	var1 = omod(S, OInteger(-90), OInteger(4));
+	CU_ASSERT_EQUAL(OAny_toInt(omod(S, OInteger(23), OInteger(2))), 1);
+	CU_ASSERT_EQUAL(OAny_toInt(omod(S, OInteger(4665), OInteger(65))), 50);
+	CU_ASSERT_EQUAL(OAny_toInt(omod(S, OInteger(-90), OInteger(4))), -2);
+	CU_ASSERT_EQUAL(OAny_toInt(omod(S, OInteger(-90), OInteger(-4))), -2);
 
 	/**add**/
 	var1 = oadd(S, OInteger(1), OInteger(2));
 	CU_ASSERT_EQUAL(OAny_toInt(var1), 3);
 
 
-
 }
 /*
- *__BEHAVIOR__.__leftshift__ = __leftshift__;
-	__BEHAVIOR__.__rightshift__ = __rightshift__;
+ *TODO BELOW
 	__BEHAVIOR__.__mod__ = __mod__;
 	__BEHAVIOR__.__bitand__ = __and__;
 	__BEHAVIOR__.__bitor__ = __or__;
 	__BEHAVIOR__.__bitxor__ = __xor__;
-	__BEHAVIOR__.__tofloat__ = __tofloat__;
 	__BEHAVIOR__.__add__ = __add__;
 	__BEHAVIOR__.__subtract__ = __subtract__;
 	__BEHAVIOR__.__divide__ = __divide__;
 	__BEHAVIOR__.__multiply__ = __multiply__;
 
+	__BEHAVIOR__.__tofloat__ = __tofloat__;
 */
