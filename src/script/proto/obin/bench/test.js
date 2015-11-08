6e19bc0a72ec7c9ed9e2c1cd2cbb4adfe18074e2
gloryofrobots <gloryofrobots@gmail.com>
2015-11-08 18:05:54 +0200

    _

--- src/script/proto/obin/bench/test.js
+++ src/script/proto/obin/bench/test.js
@@ -1,42 +1,44 @@
-
-A = {
-    action: fn (self, a1, a2){
-        return a1 + a2
-    }
-}
-
-B = A.clone()
-B.action = fn(self, a1, a2) {
-    fn _d() {
-        return a1 - a2 * 100
-    }
-    return _d()
-}
-
-a = A.action(2,3.14)
-print(a)
-b = B.action(2,3.14)
-print(b)
-
+eval("return 42")
 print("EVAL", eval("return 42 + 24"))
-x = 42
-s = "Hello"
-f = 3.1214114141414141414
-v = [x, s,  11, 3.14]
-print(v)
 
-p = x + f
-print(p)
-
-//fn F(x,y, ...rest) {
-//    print(x,y)
-//    print(rest)
+//
+//A = {
+//    action: fn (self, a1, a2){
+//        return a1 + a2
+//    }
 //}
 //
-//F(1,2, "Hello World", {name:"Bob"},[4.34, 42, "Check"])
+//B = A.clone()
+//B.action = fn(self, a1, a2) {
+//    fn _d() {
+//        return a1 - a2 * 100
+//    }
+//    return _d()
+//}
 //
-
-
-
-
-//x = ...A
+//a = A.action(2,3.14)
+//print(a)
+//b = B.action(2,3.14)
+//print(b)
+//
+//x = 42
+//s = "Hello"
+//f = 3.1214114141414141414
+//v = [x, s,  11, 3.14]
+//print(v)
+//
+//p = x + f
+//print(p)
+//
+////fn F(x,y, ...rest) {
+////    print(x,y)
+////    print(rest)
+////}
+////
+////F(1,2, "Hello World", {name:"Bob"},[4.34, 42, "Check"])
+////
+//
+//
+//
+//
+////x = ...A
