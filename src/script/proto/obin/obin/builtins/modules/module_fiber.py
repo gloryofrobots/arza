8f61658b98e161a9041f9186f13edcb56a6e90a3
gloryofrobots <gloryofrobots@gmail.com>
2016-02-23 11:24:59 +0200

    add all

---
+++ src/script/proto/obin/obin/builtins/modules/module_fiber.py
@@ -0,0 +1,27 @@
+__author__ = 'gloryofrobots'
+from obin.types import tupl, space, api
+from obin.runtime.routine import complete_native_routine
+
+def setup(process, stdlib):
+    _module = space.newemptyenv(space.newsymbol(process, u'_fiber'))
+    api.put_native_function(process, _module, u'spawn', _spawn, 1)
+    api.put_native_function(process, _module, u'activate', _activate, 2)
+
+    _module.export_all()
+    process.modules.add_module('_fiber', _module)
+
+@complete_native_routine
+def _spawn(process, routine):
+    from obin.types.fiber import newfiber
+    y1, y2 = newfiber(process)
+    return space.newtuple([y1, y2])
+
+
+def _activate(process, routine):
+    from obin.types.fiber import activate_fiber as activate
+    fiber = routine.get_arg(0)
+    func = routine.get_arg(1)
+    # args = routine.get_arg(2)
+    args = space.newtuple([space.newunit()])
+    activate(process, fiber, func, args)
+    return space.newnil()
---
+++ src/script/proto/obin/obin/builtins/modules/module_tuple.py
@@ -0,0 +1,16 @@
+__author__ = 'gloryofrobots'
+from obin.types import tupl, space, api
+from obin.runtime.routine import complete_native_routine
+
+def setup(process,  stdlib):
+    _module = space.newemptyenv(space.newsymbol(process, u'_tuple'))
+    api.put_native_function(process, _module, u'length', _length, 1)
+
+    _module.export_all()
+    process.modules.add_module('_tuple', _module)
+
+@complete_native_routine
+def _length(process, routine):
+    arg0 = routine.get_arg(0)
+
+    return tupl.length(arg0)
