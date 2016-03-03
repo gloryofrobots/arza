0c9ee22fc647c0691129580655c86d0cca527d6b
gloryofrobots <gloryofrobots@gmail.com>
2016-03-03 20:56:46 +0200

    check tuple type in UNPACK_SEQ

--- src/script/proto/obin/obin/runtime/routine/code_routine.py
+++ src/script/proto/obin/obin/runtime/routine/code_routine.py
@@ -154,6 +154,7 @@ class CodeRoutine(BaseRoutine):
             # *************************************
             elif UNPACK_SEQUENCE == tag:
                 seq = stack.pop()
+                error.affirm_type(seq, space.istuple)
                 seq_length = api.length_i(seq)
                 if seq_length != arg1:
                     return error.throw_1(error.Errors.UNPACK_SEQUENCE, seq)
