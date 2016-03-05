c039c49ae418d9c124629ca6ae95fe4449c255af
gloryofrobots <gloryofrobots@gmail.com>
2016-03-05 14:51:44 +0200

    dir structure

--- obin_py/runobin.sh
+++ obin_py/runobin.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # TODO FIX IT ON NORMAL PYTHON
 # PYTHONPATH=../../../../../../pypy python  targetobin.py test/obin/main.obn
-PYTHONPATH=../../../../../../pypy pypy  targetobin.py /home/gloryofrobots/develop/languages/obin/obin/src/script/proto/obin/test/obin/main.obn
+
+PYTHONPATH=../../../pypy pypy  targetobin.py test/obin/main.obn
